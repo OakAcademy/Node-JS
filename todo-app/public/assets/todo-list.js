2d27fcf24e37f5f59344cb0581d749ce5925c5be
OakAcademy <57633416+OakAcademy@users.noreply.github.com>
2021-03-07 15:32:16 +0300

    Add files via upload
    todo-app

---
+++ todo-app/public/assets/todo-list.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+  $('form').on('submit', function(){
+
+      var item = $('form input');
+      var todo = {item: item.val()};
+
+      $.ajax({
+        type: 'POST',
+        url: '/todo',
+        data: todo,
+        success: function(data){
+          location.reload();
+        }
+      });
+
+      return false;
+
+  });
+
+  $('li').on('click', function(){
+      var item = $(this).text().replace(/ /g, "-");
+      $.ajax({
+        type: 'DELETE',
+        url: '/todo/' + item,
+        success: function(data){
+          location.reload();
+        }
+      });
+  });
+
+});
